09770cb16feaf4819cba34990142a4495140c01b
Fengyi Li <carl.yifeng.li@outlook.com>
2021-01-06 23:40:40 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -9,22 +9,69 @@ import copy as cp
 import matplotlib.pyplot as plt
 
 
-def chain_prob_0(chainDict, flag=False):
-    # Calculate the probability for which chain will be chosen to react
-    # Applicable for reaction 2.1, 3.1, 3.2, 3.3, 4.2, 4.3 and 5.3
-    # 1, 2, 3, 4, 6, 7, 10
-    totalNum = sum(chainDict.values())
-    probChain = np.asarray(list(chainDict.values())) / totalNum
-    chainTypes = np.asarray(list(chainDict.keys()))
-    chainKey = np.random.choice(chainTypes, size=1, replace=True, p=probChain)[0]
+class myDict(dict):
+    """
+    Two functions for this simulation to run more effectively.
+    """
+    def check_existence(self, target):
+        """
+        Check if a chain is existed in the dictionary or not.
+        Input parameters
+            self: dictionary required to be checked
+            target: key for checking
+        Output parameters
+            if not existing, dictionary with added {key:0}, 
+            and otherwise return the original dictionary
+        """
+        keys = list(self.keys())
+        if target in keys:
+            pass
+        else:
+            self.update({target:0})
+
+    def delItem(self, target):
+        """
+        Delete a pair if the value = 0
+        Input parameters
+            self: dictionary required to be checked
+            target: key to check
+        """
+        if self[target] == 0:
+            del self[target]
+        else:
+            pass
+
+
+def chain_prob_0(chainDict):
+    """
+    Calculate the probability for which chain will be chosen to react
+    Applicable for reaction 2.1, 3.1, 3.2, 3.3, 4.2, 4.3 and 5.3 (one polymer with n monomers)
+    reaction number = 1, 2, 3, 4, 6, 7, 10
+    Input parameters
+        chainDict: dictionary storing n and chain number
+    Output parameters
+        n of the chain reacting
+    """
+    totalNum = sum(chainDict.values()) #calculate the total number of chains
+    probChain = np.asarray(list(chainDict.values())) / totalNum #calculate the probability
+    chainTypes = np.asarray(list(chainDict.keys())) #make all of the keys into a numpy array
+    chainKey = np.random.choice(chainTypes, size=1, replace=True, p=probChain)[0] #random choose a chain to react according to the probabilty
     
     return chainKey
 
 
 def chain_prob_1(chainDict_m, chainDict_n, flag=False):
-    # Calculate the probability for which chains combination will react
-    # Applicable for reaction 4.1, 5.1, 5.2
-    # 5, 8, 9, 11
+    """
+    Calculate the probability for which chains combination will react
+    Applicable for reaction 4.1, 5.1, 5.2 (two polymers with n and m monomers respectively)
+    reaction number = 5, 8, 9, 11
+    Input parameters
+        chainDict_m: dictionary storing chain type and chain number
+        chainDict_n: dictionary storing chain type and chain number
+        flag: True for reaction containing two variables and False for only one
+    Output parameters
+        [one key for chainDict_m, one key for chainDict_n]
+    """
     chainNums_m = np.asarray(list(chainDict_m.values()))
     chainTypes_m = np.asarray(list(chainDict_m.keys()))
     chainNums_n = np.asarray(list(chainDict_n.values()))
@@ -44,6 +91,7 @@ def chain_prob_1(chainDict_m, chainDict_n, flag=False):
     else: # reaction 4.1 and 5.4
         pass
 
+    #calculate the probability
     prob = num_product[:,0] * num_product[:,1]
     probabilities = prob / sum(num_product[:,0] * num_product[:,1])
     combination_index = np.random.choice(np.arange(len(combinations)), size=1, 
@@ -52,99 +100,82 @@ def chain_prob_1(chainDict_m, chainDict_n, flag=False):
     return combinations[combination_index] # [n, m]
 
 
-def check_existence(inputDict, target):
-    keys = list(inputDict.keys())
-    if target in keys:
-        pass
-    else:
-        inputDict.update({target:0})
-    
-    return inputDict
-
-
-def delItem(dictionary, target):
-    if dictionary[target] == 0:
-        del dictionary[target]
-    else:
-        pass
-
-    return dictionary
-
-
+# reaction 1.1
 def reaction11():
     global I, Rn_d
     I -= 1
-    Rn_d = check_existence(Rn_d, 0)
+    #check the existence of R0 and update the value of it
+    Rn_d.check_existence(0)
     Rn_d.update({0: Rn_d[0]+2})
 
-
+#reaction 2.1, 3.1, 3.2, 3.3, 4.2, 4.3
 def reaction_prob_0(num):
     # Combine reaction 2.1, 3.1, 3.2 and 3.3 together
     # Reaction 2.1 and 3.1
     global M, T, Rn_d, d_TRn, TRn, RnTRm
     if num in [1, 2]:
-        reacts = chain_prob_0(Rn_d) # n
+        reacts = chain_prob_0(Rn_d) # returns which n to react
         Rn_d.update({reacts: Rn_d[reacts]-1})
-        Rn_d = delItem(Rn_d, reacts)
+        Rn_d.delItem(reacts)
         if num == 1: # reaction 2.1
             M -= 1
             newChain = reacts + 1
-            Rn_d = check_existence(Rn_d, newChain)
+            Rn_d.check_existence(newChain)
             Rn_d.update({newChain: Rn_d[newChain]+1})
         else: # reaction 3.1
             T -= 1
-            d_TRn = check_existence(d_TRn, reacts)
+            d_TRn.check_existence(reacts)
             d_TRn.update({reacts: d_TRn[reacts]+1})
     # reaction 3.2 and 3.3
     elif num in [3, 4]:
         reacts = chain_prob_0(d_TRn)
         d_TRn.update({reacts: d_TRn[reacts]-1})
-        d_TRn = delItem(d_TRn, reacts)
+        d_TRn.delItem(reacts)
         if num == 3: # reaction 3.2
-            Rn_d = check_existence(Rn_d, reacts)
+            Rn_d.check_existence(reacts)
             Rn_d.update({reacts: Rn_d[reacts]+1})
             T += 1
         else: # reaction 3.3
-            Rn_d = check_existence(Rn_d, 0)
+            Rn_d.check_existence(0)
             Rn_d.update({0: Rn_d[0]+1})
-            TRn = check_existence(TRn, reacts)
+            TRn.check_existence(reacts)
             TRn.update({reacts: TRn[reacts]+1})
     else: # reaction 4.2 and 4.3
         reacts = chain_prob_0(RnTRm) # string 'n,m'
         RnTRm.update({reacts: RnTRm[reacts]-1})
-        RnTRm = delItem(RnTRm, reacts)
+        RnTRm.delItem(reacts)
         n, m = int(list(reacts)[0]), int(list(reacts[2]))
         if num == 6: # reaction 4.2
-            Rn_d = check_existence(Rn_d, n)
+            Rn_d.check_existence(n)
             Rn_d.update({n: Rn_d[n]+1})
-            TRn = check_existence(TRn, m)
+            TRn.check_existence(m)
             TRn.update({m: TRn[m]+1})
         else: # reaction 4.3
-            Rn_d = check_existence(Rn_d, m)
+            Rn_d.check_existence(m)
             Rn_d.update({m: Rn_d[m]+1})
-            TRn = check_existence(TRn, n)
+            TRn.check_existence(n)
             TRn.update({n: TRn[n]+1})
 
-
+#reaction 4.1, 5.1, 5.2
 def reaction_prob_1(num):
     global Rn_d, TRn, RnTRm
     if num == 5: # reaction 4.1
         reacts = chain_prob_1(Rn_d, TRn)
         n, m = reacts[0], reacts[1]
         Rn_d.update({n: Rn_d[n]-1})
-        Rn_d = delItem(Rn_d, n)
+        Rn_d.delItem(n)
         TRn.update({m: TRn[m]-1})
-        TRn = delItem(TRn, m)
+        TRn.delItem(m)
         newChain = str(n)+','+str(m)
-        RnTRm = check_existence(RnTRm, newChain)
+        RnTRm.check_existence(newChain)
         RnTRm.update({newChain: RnTRm[newChain]+1})
     else: # reaction 5.1 or 5.2
         reacts = chain_prob_1(Rn_d, Rn_d, True)
         n, m = reacts[0], reacts[1]
         Rn_d.update({n: Rn_d[n]-1})
-        Rn_d = delItem(Rn_d, n)
+        Rn_d.delItem(n)
         Rn_d.update({m: Rn_d[m]-1})
-        Rn_d = delItem(Rn_d, m)
+        Rn_d.delItem(m)
 
         if num == 8: # reaction 5.1
             """
@@ -154,6 +185,7 @@ def reaction_prob_1(num):
             """
             For updating the concentration of the Pn and Pm
             """
+
 
 global M, Rn_d, d_TRn, TRn, RnTRm
 # Global constants required to be tuned before starting the simulation
@@ -207,17 +239,18 @@ for IandT in initial_concentrations:
     T0 = int(IandT[1] * 1E-3 * V * NA)
     M0 = int(M * V * NA)
     # print(I0)
-    t = 0               # System time  (s)
-    I = I0              # Initial initiator concentration  (#)
-    T = T0              # Initial agent concentration      (#)
-    M = M0              # Initial monomer concentration    (#)
-    Rn_d = {0:0}        # Rn dot : number of chains
-    d_TRn = {}          # dot TRn : number of chains
-    TRn = {}    
-    RnTRm = {}  
-    times = 0           # variable to update the name of the txt file
-    wDict = {}          # store the data required for distribution plots
-    t_and_R0 = {0:0}    # store the data required for concentration plots
+    t = 0                       # System time  (s)
+    I = I0                      # Initial initiator concentration  (#)
+    T = T0                      # Initial agent concentration      (#)
+    M = M0                      # Initial monomer concentration    (#)
+    Rn_d = myDict({0:0})        # Rn dot : number of chains
+    d_TRn = myDict()            # dot TRn : number of chains
+    TRn = myDict()    
+    RnTRm = myDict()  
+    times = 0                   # variable to update the name of the txt file
+    wDict = myDict()            # store the data required for distribution plots
+    t_and_R0 = myDict({0:0})    # store the data required for concentration plots
+
     # A specific initial setting
     while(t < t_max):
         ratesList = [] # a list of reaction rate
@@ -286,13 +319,13 @@ for IandT in initial_concentrations:
         print('tau', tau)
         
 
-        # Step4: choose the exact chain to react
+        # Step 4: choose the exact chain to react
         if reaction_chosen == 0:
             reaction11()
-        elif reaction_chosen in [1, 2, 3, 4, 6, 7, 10]:
-            reaction_prob_0(reaction_chosen)
-        else:
+        elif reaction_chosen in [5, 8, 9, 11]:
             reaction_prob_1(reaction_chosen)
+        else:
+            reaction_prob_0(reaction_chosen)
         
         # collect concentration of the R0
         if 0 not in Rn_d.keys():
